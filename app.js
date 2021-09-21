4d3150c18004818d1703c52ce074eaae86302340
indium-dinesh <dinesh.subramani@indiumsoft.com>
2021-09-21 21:17:33 +0530

    trying to change master

--- app.js
+++ app.js
@@ -4,4 +4,22 @@ var port = process.env.PORT || 3000
 
 app.use( express.static('public') )
 
+
+app.use('/journey/execute/',(req,res)=>{
+    console.log("execute");
+    res.send({"status":"ok"})
+});
+app.use('/journey/save/', (req,res)=>{
+    console.log("save");
+    res.send({"status":"ok"})
+});
+app.use('/journey/publish/',(req,res)=>{
+    console.log("publish");
+    res.send({"status":"ok"})
+});
+app.use('/journey/validate/',(req,res)=>{
+    console.log("validate");
+    res.send({"status":"ok"})
+});
+
 app.listen( port, () => console.log( `App listening on port ${port}!`) )
