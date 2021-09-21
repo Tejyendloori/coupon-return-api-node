2b4824a070c187d797cb3379d4cee24608bfcd52
indium-dinesh <dinesh.subramani@indiumsoft.com>
2021-09-21 17:46:59 +0530

    update jwt

--- utils/jwtDecoder.js
+++ utils/jwtDecoder.js
@@ -6,7 +6,7 @@ module.exports = (body) => {
     return new Error('invalid jwtdata');
   }
 
-  return JWT.verify(body.toString('utf8'), "secret", {
+  return JWT.verify(body.toString('utf8'), "ECHmFz1CzkFlQaNKQau7cYP838_BAYpBGsn6UEWTZVZO1kYxCjoVIX47YACwJmyP_dYoiWxE4DZSJDc4-INFonuquxWZqGqHhtF--oLBGL6vUxh_FUGLuNKb4YdN9wlhD9TUwbNglT1JUJxP95s2wJoCv8RPEHwSZn87gauksPOrQjSSTaQRPU5Hh-eYgRUluSESkakeGCv1iQ9V6Pmr-R7RTxDSVnyUWbb6xs3THQQpLazdS8WM4itJPTkPNw2", {
     algorithm: 'HS256',
   });
 };
