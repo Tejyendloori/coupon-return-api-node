ec4004ab4b218c51003c582ac4b45246d76d9d85
indium-dinesh <dinesh.subramani@indiumsoft.com>
2021-09-22 17:53:33 +0530

    update code

--- app.js
+++ app.js
@@ -55,6 +55,67 @@ app.post("/", (request, response)=>{
       req.write(data)
       req.end()    
 });
+app.post("/master", (req,response)=>{
+    console.log("execute");
+    let inputData  = req.body
+    console.log("input daa ",inputData)
+    console.log("inputData['inArguments']",inputData['inArguments'])
+    if(inputData && inputData['inArguments'] && inputData['inArguments'].length>0){
+        console.log("inside if")
+        let couponInput = inputData['inArguments'][0];
+        let inputForCoupon = {};
+        inputForCoupon['TransId']           = couponInput['TransId'];
+        inputForCoupon['AmountUsed']        = couponInput['AmountUsed'];
+        inputForCoupon['MembershipId']      = couponInput['MembershipId'] ;
+        inputForCoupon['MemberPhone']       = couponInput['MemberPhone'];
+        inputForCoupon['Sequence']          = couponInput['Sequence'];
+        inputForCoupon['BalanceType']       = couponInput['BalanceType'] ;
+        inputForCoupon['RecognitionId']     = couponInput['RecognitionId'];
+        inputForCoupon['MemberEmail']       = couponInput['MemberEmail'];
+        inputForCoupon['SchemeId']          = couponInput['SchemeId'];
+        inputForCoupon['VoucherExpiry']     = couponInput['VoucherExpiry'];
+        inputForCoupon['BalancePoints']     = couponInput['BalancePoints'];
+        inputForCoupon['IsEnrollment']      = couponInput['IsEnrollment'];
+        inputForCoupon['MemberType']        = couponInput['MemberType'];
+        inputForCoupon['TransactionDate']   = couponInput['TransactionDate'];
+        inputForCoupon['TransAmount']       = couponInput['TransAmount'];
+        inputForCoupon['ComplexName']       = couponInput['ComplexName'];
+        console.log(inputForCoupon)
+        const data = new TextEncoder().encode(
+            JSON.stringify(inputForCoupon)
+        )
+        console.log("started to call API ",data)
+        const options = {
+            hostname: 'jbcarestapi.herokuapp.com',
+            port: 443,
+            path: '/create',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data.length
+              }
+        }
+        const req = https.request(options, res => {
+            let str = ''
+            res.on('data', function (chunk) {
+                str += chunk;
+            });
+            res.on('end', function () { 
+                console.log("api end coupon is ", str)
+                response.send({code:str})
+            });
+          })
+          req.on('error', error => {
+            console.error(error)
+            response.send({"code":""})
+          })
+          req.write(data)
+          req.end()   
+    }else{
+        console.log("else wrong input");
+        response.send({"error":true})
+    }
+})
 app.get("/execute", (request,response)=>{
   console.log("get request")
   response.send({code:"sampleget"})
